a63cbed3d923bb9705e07fc846befa27a31fcdc3
Navruzbekashurov <navruzashurov0321@gmail.com>
2025-06-05 13:37:23 +0500

    add functions

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $createdAt = Carbon::instance(fake()->dateTimeBetween('-2 years'));
+
+        return [
+            'title'=>fake()->sentence(3),
+            'author'=>fake()->name,
+            'created_at'=>$createdAt,
+            'updated_at'=>fake()->dateTimeBetween($createdAt, 'now')
+        ];
+    }
+}
