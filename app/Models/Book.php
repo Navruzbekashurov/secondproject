34570d1229c57b1f66de1f8d2fa924217f669af2
Navruzbekashurov <navruzashurov0321@gmail.com>
2025-06-05 13:37:17 +0500

    add functions

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -10,11 +12,46 @@ class Book extends Model
 {
 
     use HasFactory;
+
     //
 
     public function reviews(): HasMany
     {
         return $this->hasMany(Review::class);
+
+    }
+
+    public function scopeTitle(Builder $builder, string $title): Builder
+    {
+        return $builder->where('title', 'like', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withCount(['reviews' => fn(Builder $q) => $this->dataRangeFilter($q, $from, $to)])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dataRangeFilter($q, $from, $to)], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dataRangeFilter(Builder $query, $from = null, $to = null): void
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\BookControler;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
     return view('welcome');
+
+    //Book::truncate();
+    //\App\Models\Book::factory(100000)->create();
+  // return \App\Models\Book::all();
+//     \Illuminate\Support\Facades\Cache::set('books', $books,50);
+  //  return \Illuminate\Support\Facades\Cache::get('books');
 });
+
+Route::resource('books', BookControler::class);
